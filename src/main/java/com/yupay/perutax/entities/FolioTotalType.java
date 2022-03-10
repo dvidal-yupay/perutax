d508b68f9e521a2b82a89b7648eb19880e7adb58
dvidal <dvidal@infoyupay.com>
2022-03-09 21:54:08 -0500

    Enumeration FolioTotalType implemented.

---
+++ src/main/java/com/yupay/perutax/entities/FolioTotalType.java
@@ -0,0 +1,84 @@
+/*
+ *  Perutax - Taxation software for Peru.
+ *     Copyright (C) 2021-2022  Ingenieria Informatica Yupay SACS
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.yupay.perutax.entities;
+
+/**
+ * This enumeration represents the distinct
+ * types of items to show in the totals
+ * footer of a given folio.
+ *
+ * @author InfoYupay SACS
+ * @version 1.0
+ */
+public enum FolioTotalType {
+    /**
+     * The taxable amount.
+     */
+    TAXABLE("Op. Gravada"),
+    /**
+     * The tax amount.
+     */
+    TAX("IGV"),
+    /**
+     * The discount amount.
+     */
+    DISCOUNT("Descuento"),
+    /**
+     * The tax of the discount amount.
+     */
+    DISCOUNT_TAX("Dscto. - IGV"),
+    /**
+     * Exportation amount.
+     */
+    EXPORT("Exportación"),
+    /**
+     * Tax exempt amount.
+     */
+    EXEMPT("Exonerado"),
+    /**
+     * Tax free amount.
+     */
+    TAX_FREE("Inafecto"),
+    /**
+     * ICBP tax (bag plastics tax).
+     */
+    ICBP("ICBP"),
+    /**
+     * Others.
+     */
+    OTHERS("Otros");
+    /**
+     * The human-friendly text to use in GUI.
+     */
+    private final String title;
+
+    /**
+     * The default constructor.
+     *
+     * @param title a brief title to show to user.
+     */
+    FolioTotalType(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+}
